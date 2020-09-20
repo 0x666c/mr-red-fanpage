39e4b4938155110f0d5ae53ad3fbe572e1178802
0x666c <0x666c@mail.ru>
2020-09-20 23:14:45 +0300

    Updates

---
+++ precache-manifest.d5a86ea3a0e3bd9a43a310806e934d47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0420e716ee15b9f2fa96ba50de23c74",
+    "url": "/mr-red-fanpage/index.html"
+  },
+  {
+    "revision": "44cac7667d82c3cd3fbc",
+    "url": "/mr-red-fanpage/static/css/main.20786036.chunk.css"
+  },
+  {
+    "revision": "ff73954e3113e322b253",
+    "url": "/mr-red-fanpage/static/js/2.404d994b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mr-red-fanpage/static/js/2.404d994b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cac7667d82c3cd3fbc",
+    "url": "/mr-red-fanpage/static/js/main.e901ddae.chunk.js"
+  },
+  {
+    "revision": "54c2a6c9d8237eb96c9a",
+    "url": "/mr-red-fanpage/static/js/runtime-main.d5e9554f.js"
+  }
+]);
